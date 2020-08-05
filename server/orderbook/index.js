66e984417ffe0495091818826baa94acff984667
winter520 <WDwq681227>
2020-08-05 21:38:48 +0800

    optimzed calculate

--- server/orderbook/index.js
+++ server/orderbook/index.js
@@ -39,83 +39,146 @@ const web3 = require(pathLink + '/server/public/web3/index.js')
 //   console.log(res)
 // })
 
-function getAmount (depth, pair, pecent = 200) {
-  let data = {
-    bids: [],
-    asks: []
-  }
-  depth = depth.toString()
-  depth = ethers.utils.parseUnits(depth, coinInfo[pair].dec)
-  depth = depth.mul(999).div(1000)
+// function getAmount (depth, pair, pecent = 200) {
+//   let data = {
+//     bids: [],
+//     asks: []
+//   }
+//   depth = depth.toString()
+//   depth = ethers.utils.parseUnits(depth, coinInfo[pair].dec)
+//   depth = depth.mul(999).div(1000)
+//   return new Promise(resolve => {
+//     let exchangeContract = new web3.eth.Contract(EXCHANGE, coinInfo[pair].exchange)
+//     async.waterfall([
+//       (cb) => {
+//         exchangeContract.methods.getEthToTokenInputPrice(depth).call((err, res) => {
+//           let balance = 0
+//           if (!err) {
+//             balance = res
+//           }
+//           // console.log('max')
+//           // console.log(balance)
+//           cb(null, balance)
+//         })
+//       },
+//       (max, cb) => {
+//         exchangeContract.methods.getEthToTokenInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
+//           let balance = 0
+//           if (!err) {
+//             balance = res
+//           }
+//           // console.log('min')
+//           // console.log(balance)
+//           data.bids = [
+//             [
+//               (Number(depth.toString()) / Number(max.toString())).toFixed(5)
+//               , $$.fromWei(max, coinInfo[pair].dec)
+//             ],
+//             [
+//               (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
+//               , $$.fromWei(balance, coinInfo[pair].dec)
+//             ],
+//           ]
+//           cb(null, data)
+//         })
+//       },
+//       (results, cb) => {
+//         exchangeContract.methods.getTokenToEthInputPrice(depth).call((err, res) => {
+//           let balance = 0
+//           if (!err) {
+//             balance = res
+//           }
+//           // console.log('max')
+//           // console.log(balance)
+//           cb(null, balance)
+//         })
+//       },
+//       (max, cb) => {
+//         exchangeContract.methods.getTokenToEthInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
+//           let balance = 0
+//           if (!err) {
+//             balance = res
+//           }
+//           // console.log('min')
+//           // console.log(balance)
+//           data.asks = [
+//             [
+//               (Number(depth.toString()) / Number(max.toString())).toFixed(5)
+//               , $$.fromWei(max, coinInfo[pair].dec)
+//             ],
+//             [
+//               (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
+//               , $$.fromWei(balance, coinInfo[pair].dec)
+//             ],
+//           ]
+//           cb(null, data)
+//         })
+//       }
+//     ], (err, res) => {
+//       resolve(data)
+//     })
+//   })
+// }
+
+function calculateBuyAndSell (x, y, pecent) {
+  pecent = Number(pecent) / 10000
+  // x = Number($$.fromWei(x))
+  // y = Number($$.fromWei(y))
+  let fee = 0.004
+  let a = Number(y)
+  let b = 2 * y * x - (1 + pecent) * fee * x * y
+  let c = -pecent * x * x *y
+  let result = b * b - 4 * a * c
+  let result1 = -b/(2*a)+Math.sqrt(result)/(2*a)
+  let markets = x / y
+  return [markets.toFixed(5), result1]
+}
+
+function getAmount (depth, pair) {
   return new Promise(resolve => {
-    let exchangeContract = new web3.eth.Contract(EXCHANGE, coinInfo[pair].exchange)
     async.waterfall([
       (cb) => {
-        exchangeContract.methods.getEthToTokenInputPrice(depth).call((err, res) => {
+        let contract = new web3.eth.Contract(ERC20, coinInfo[pair].token)
+        contract.methods.balanceOf(coinInfo[pair].exchange).call({from: coinInfo[pair].exchange}, (err, res) => {
           let balance = 0
           if (!err) {
             balance = res
           }
-          // console.log('max')
-          // console.log(balance)
+          // console.log(res)
           cb(null, balance)
         })
       },
-      (max, cb) => {
-        exchangeContract.methods.getEthToTokenInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
-          let balance = 0
-          if (!err) {
-            balance = res
-          }
-          // console.log('min')
-          // console.log(balance)
-          data.bids = [
-            [
-              (Number(depth.toString()) / Number(max.toString())).toFixed(5)
-              , $$.fromWei(max, coinInfo[pair].dec)
-            ],
-            [
-              (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
-              , $$.fromWei(balance, coinInfo[pair].dec)
-            ],
-          ]
-          cb(null, data)
+      (balance, cb) => {
+        web3.eth.getBalance(coinInfo[pair].exchange).then(res => {
+          // console.log(res)
+          cb(null, {
+            fsn: res,
+            token: balance
+          })
         })
       },
-      (results, cb) => {
-        exchangeContract.methods.getTokenToEthInputPrice(depth).call((err, res) => {
-          let balance = 0
-          if (!err) {
-            balance = res
-          }
-          // console.log('max')
-          // console.log(balance)
-          cb(null, balance)
-        })
-      },
-      (max, cb) => {
-        exchangeContract.methods.getTokenToEthInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
-          let balance = 0
-          if (!err) {
-            balance = res
-          }
-          // console.log('min')
-          // console.log(balance)
-          data.asks = [
-            [
-              (Number(depth.toString()) / Number(max.toString())).toFixed(5)
-              , $$.fromWei(max, coinInfo[pair].dec)
-            ],
-            [
-              (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
-              , $$.fromWei(balance, coinInfo[pair].dec)
-            ],
+      (obj, cb) => {
+        let x = Number($$.fromWei(obj.fsn))
+        let y = Number($$.fromWei(obj.token))
+        let data = {
+          ticker_id: pair + '_FSN',
+          timestamp: parseInt(Date.now() / 1000),
+          bids: [
+            calculateBuyAndSell(x, y, depth / 2),
+            calculateBuyAndSell(x, y, depth)
+          ],
+          asks: [
+            calculateBuyAndSell(y, x, depth / 2),
+            calculateBuyAndSell(y, x, depth)
           ]
-          cb(null, data)
-        })
+        }
+        // console.log(calculateBuyAndSell(x, y, depth))
+        cb(null, data)
       }
     ], (err, res) => {
-      resolve(data)
+      console.log('res')
+      console.log(res)
+      resolve(res)
     })
   })
 }
