be667370282f8fd0a33523f84aa1fee443d599e2
winter520 <WDwq681227>
2020-08-21 22:47:04 +0800

    add dec

--- server/orderbook/index.js
+++ server/orderbook/index.js
@@ -16,114 +16,9 @@ const SWAPETHABI = require(pathLink + '/server/public/ABI/swapETHABI.js')
 const ethers = require('ethers')
 const web3 = require(pathLink + '/server/public/web3/index.js')
 
-// function calculateEtherTokenOutputFromInput(inputAmount, inputReserve, outputReserve) {
-//   inputReserve = ethers.utils.bigNumberify(inputReserve)
-//   outputReserve = ethers.utils.bigNumberify(outputReserve)
-//   const inputAmountWithFee = inputAmount.mul(ethers.utils.bigNumberify(997))
-//   const numerator = inputAmountWithFee.mul(outputReserve)
-//   const denominator = inputReserve.mul(ethers.utils.bigNumberify(1000)).add(inputAmountWithFee)
-//   return numerator.div(denominator)
-// }
-
-
-
-// let contract = new web3.eth.Contract(ERC20, coinInfo['ANY'].token)
-// contract.methods.balanceOf(coinInfo['ANY'].exchange).call({from: coinInfo['ANY'].exchange}, (err, res) => {
-//   let balance = 0
-//   if (!err) {
-//     balance = res
-//   }
-//   console.log(res)
-// })
-// web3.eth.getBalance(coinInfo['ANY'].exchange).then(res => {
-//   console.log(res)
-// })
-
-// function getAmount (depth, pair, pecent = 200) {
-//   let data = {
-//     bids: [],
-//     asks: []
-//   }
-//   depth = depth.toString()
-//   depth = ethers.utils.parseUnits(depth, coinInfo[pair].dec)
-//   depth = depth.mul(999).div(1000)
-//   return new Promise(resolve => {
-//     let exchangeContract = new web3.eth.Contract(EXCHANGE, coinInfo[pair].exchange)
-//     async.waterfall([
-//       (cb) => {
-//         exchangeContract.methods.getEthToTokenInputPrice(depth).call((err, res) => {
-//           let balance = 0
-//           if (!err) {
-//             balance = res
-//           }
-//           // console.log('max')
-//           // console.log(balance)
-//           cb(null, balance)
-//         })
-//       },
-//       (max, cb) => {
-//         exchangeContract.methods.getEthToTokenInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
-//           let balance = 0
-//           if (!err) {
-//             balance = res
-//           }
-//           // console.log('min')
-//           // console.log(balance)
-//           data.bids = [
-//             [
-//               (Number(depth.toString()) / Number(max.toString())).toFixed(5)
-//               , $$.fromWei(max, coinInfo[pair].dec)
-//             ],
-//             [
-//               (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
-//               , $$.fromWei(balance, coinInfo[pair].dec)
-//             ],
-//           ]
-//           cb(null, data)
-//         })
-//       },
-//       (results, cb) => {
-//         exchangeContract.methods.getTokenToEthInputPrice(depth).call((err, res) => {
-//           let balance = 0
-//           if (!err) {
-//             balance = res
-//           }
-//           // console.log('max')
-//           // console.log(balance)
-//           cb(null, balance)
-//         })
-//       },
-//       (max, cb) => {
-//         exchangeContract.methods.getTokenToEthInputPrice(depth.mul(10000 - pecent).div(10000)).call((err, res) => {
-//           let balance = 0
-//           if (!err) {
-//             balance = res
-//           }
-//           // console.log('min')
-//           // console.log(balance)
-//           data.asks = [
-//             [
-//               (Number(depth.toString()) / Number(max.toString())).toFixed(5)
-//               , $$.fromWei(max, coinInfo[pair].dec)
-//             ],
-//             [
-//               (Number(depth.toString()) / Number(balance.toString())).toFixed(5)
-//               , $$.fromWei(balance, coinInfo[pair].dec)
-//             ],
-//           ]
-//           cb(null, data)
-//         })
-//       }
-//     ], (err, res) => {
-//       resolve(data)
-//     })
-//   })
-// }
 
 function calculateBuy (x, y, pecent) {
   pecent = Number(pecent) / 10000
-  // x = Number($$.fromWei(x))
-  // y = Number($$.fromWei(y))
   let fee = 0.004
   let a = Number(y)
   let b = 2 * y * x - (1 + pecent) * fee * x * y
@@ -136,8 +31,6 @@ function calculateBuy (x, y, pecent) {
 
 function calculateSell (x, y, pecent) {
   pecent = -Number(pecent) / 10000
-  // x = Number($$.fromWei(x))
-  // y = Number($$.fromWei(y))
   let fee = 0.004
   let a = Number(y)
   let b = 2 * y * x - (1 + pecent) * fee * x * y
@@ -175,7 +68,7 @@ function getAmount (depth, pair) {
       },
       (obj, cb) => {
         let x = Number($$.fromWei(obj.fsn))
-        let y = Number($$.fromWei(obj.token))
+        let y = Number($$.fromWei(obj.token, coinInfo[pair].dec))
         let data = {
           ticker_id: pair + '_FSN',
           timestamp: parseInt(Date.now() / 1000).toString(),
